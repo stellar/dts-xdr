876126aa93306caf97f2fa68f52736dd2e5d3104
Adolfo Builes <builes.adolfo@gmail.com>
2020-11-13 13:56:35 -0500

    Include definitions for validateXDR added in https://github.com/stellar/js-xdr/pull/56. (#2)

--- src/xdr-types/io-mixin.js
+++ src/xdr-types/io-mixin.js
@@ -77,4 +77,36 @@ export default function ioMixin(
       dom.DeclarationFlags.Static
     )
   );
+  node.members.push(
+    dom.create.method(
+      'validateXDR',
+      [
+        dom.create.parameter('input', buffer),
+        dom.create.parameter(
+          'format',
+          dom.type.stringLiteral('raw'),
+          dom.ParameterFlags.Optional
+        )
+      ],
+      dom.type.boolean,
+      dom.DeclarationFlags.Static
+    )
+  );
+  node.members.push(
+    dom.create.method(
+      'validateXDR',
+      [
+        dom.create.parameter('input', dom.type.string),
+        dom.create.parameter(
+          'format',
+          dom.create.union([
+            dom.type.stringLiteral('hex'),
+            dom.type.stringLiteral('base64')
+          ])
+        )
+      ],
+      dom.type.boolean,
+      dom.DeclarationFlags.Static
+    )
+  );
 }
